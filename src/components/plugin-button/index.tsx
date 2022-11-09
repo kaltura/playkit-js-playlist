9c1c5e25d3113ed083b6d2c364805abb232828a0
Serhii <serhii.marchenko@Kaltura.com>
2022-11-09 11:16:10 +0200

    feat(FEV-1551): Add support to the More plugin (#20)
    * feat(FEV-1551): upgrade ui-managers (more plugin support)
    
    * feat(FEV-1551): tech fixes
    
    * feat(FEV-1551): use player-ui styles for icon
    
    * feat(FEV-1551): add ui-manager dependency

--- src/components/plugin-button/index.tsx
+++ src/components/plugin-button/index.tsx
@@ -1,6 +1,8 @@
 import {h} from 'preact';
+import {A11yWrapper, OnClick} from '@playkit-js/common';
 import {icons} from '../icons';
 import * as styles from './plugin-button.scss';
+import {ui} from 'kaltura-player-js';
 
 const {Tooltip, Icon} = KalturaPlayer.ui.components;
 const {withText, Text} = KalturaPlayer.ui.preacti18n;
@@ -13,22 +15,24 @@ const translates = ({isActive}: PluginButtonProps) => {
 
 interface PluginButtonProps {
   isActive: boolean;
-  onClick: () => void;
+  onClick: OnClick;
   label?: string;
 }
 
 export const PluginButton = withText(translates)(({isActive, onClick, ...otherProps}: PluginButtonProps) => {
   return (
     <Tooltip label={otherProps.label} type="bottom">
-      <button aria-label={otherProps.label} className={[styles.pluginButton, isActive ? styles.active : ''].join(' ')} onClick={onClick}>
-        <Icon
-          id="playlist-plugin-button"
-          height={icons.BigSize}
-          width={icons.BigSize}
-          viewBox={`0 0 ${icons.BigSize} ${icons.BigSize}`}
-          path={icons.PLUGIN_ICON}
-        />
-      </button>
+      <A11yWrapper onClick={onClick}>
+        <button aria-label={otherProps.label} className={[ui.style.upperBarIcon, styles.pluginButton, isActive ? styles.active : ''].join(' ')}>
+          <Icon
+            id="playlist-plugin-button"
+            height={icons.BigSize}
+            width={icons.BigSize}
+            viewBox={`0 0 ${icons.BigSize} ${icons.BigSize}`}
+            path={icons.PLUGIN_ICON}
+          />
+        </button>
+      </A11yWrapper>
     </Tooltip>
   );
 });
